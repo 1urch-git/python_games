2ec765478e259dc423ee008e4b68fd36361dcfe4
1urch <charleshreed10@gmail.com>
2023-02-16 00:35:45 -0500

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -2,11 +2,32 @@
 
 import os, sys
 
-def board():
-    print("[ ] [ ] [ ]\n")
-    print("[ ] [ ] [ ]\n")
-    print("[ ] [ ] [ ]\n")
+row1 = "[ ] [ ] [ ]\n"
+row2 = "[ ] [ ] [ ]\n"
+row3 = "[ ] [ ] [ ]\n"
+
+row_value1 = []
+row_value2 = []
+row_value3 = []
+    
+def intro():
+    print("Welcome to tic-tac-toe! Get 3 in a row to win.\n")
+    
+def help():
+    row_value1 = ["[1] ","[2] ","[3]"]
+    row_value2 = ["[4] ","[5] ","[6]"]
+    row_value3 = ["[7] ","[8] ","[9]"]
+    print("Here is how to play: pick a number from the available spots. The slots are numbered like this...\n")
+    
+    #add update_board() here
+    println(row1, row2, row3)
+
+def show_board():
+    print(row1, row2, row3)
+    
+def assign_values():
+    input(
     
 if __name__ == "__main__":
     #do something
-    board()
+    show_board()
