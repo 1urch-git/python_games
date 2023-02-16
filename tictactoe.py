34be88d85ea0ced33cf185d4aeb1597b1c5de607
1urch <charleshreed10@gmail.com>
2023-02-15 23:57:26 -0500

    Create tictactoe.py

---
+++ tictactoe.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+
+import os, sys
+
+def board():
+    print("[ ] [ ] [ ]\n")
+    print("[ ] [ ] [ ]\n")
+    print("[ ] [ ] [ ]\n")
+    
+if __name__ == "__main__":
+    #do something
+    board()
